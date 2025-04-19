d3bde928501aebc9979e9c5d852b1fb93d91abe5
miwanoff <marina.iva.noffa@gmail.com>
2025-04-19 15:03:17 +0300

    Add  loadWords JSON

---
+++ script1.js
@@ -0,0 +1,50 @@
+function goWithWords(words) {
+  let word = words[2].translation;
+  console.log(word);
+
+  let remain = word.length - 2;
+
+  let answer = []; // що відкрито
+  answer[0] = word[0]; // перша літера буде відкрита
+  answer[word.length - 1] = word[word.length - 1]; // остання літера буде відкрита
+
+  for (let i = 1; i < word.length - 1; i++) {
+    answer[i] = "_"; // між першою та останньою літерою - знаки підкреслення
+  }
+
+  let answElement = document.getElementById("answ");
+
+  answElement.innerHTML = answer.join(" ");
+
+  let remainElement = document.getElementById("remain");
+  remainElement.innerHTML = remain;
+
+  function guessLetter() {
+    let guess = prompt("Введіть літеру");
+    for (let i = 0; i < word.length; i++) {
+      // проходимо рядок word поелементно, як масив
+      if (word[i] === guess) {
+        // якщо літера рядка збіглася з вгаданою
+        answer[i] = guess; // записуємо її до масиву відкритих букв
+        remain--; // зменшуємо на 1 кількість спроб, що залишилися.
+      }
+    }
+  }
+}
+function loadWords() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "dictionary.json", true);
+  xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (xhttp.readyState != 4) return;
+    if (xhttp.status != 200) {
+      alert(xhttp.status + ": " + xhr.statusText);
+    } else {
+      let words = JSON.parse(xhttp.responseText);
+      goWithWords(words);
+    }
+  };
+}
+
+loadWords();
